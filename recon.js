3f4c34e6a781139544d2e694db233a7da2352726
App Academy Student <kithaca@gmail.com>
2016-03-08 21:44:30 -0800

    bases, obstacles, flag

--- recon.js
+++ recon.js
@@ -2,8 +2,8 @@ var GameView = require('./lib/gameView');
 var Game = require('./lib/game');
 
 var canvas = document.getElementById('canvas');
-canvas.width = 1200;
-canvas.height = 1000;
+canvas.width = 1300;
+canvas.height = 950;
 
 var ctx = canvas.getContext('2d');
 ctx.backgroundColor = "midnightblue"
